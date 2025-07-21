777e10cca81218d5d5e5a8adc268874ad2e27589
Erikplacido <erik.placido@gmail.com>
2025-07-21 09:11:35 -0300

    Delete booking directory

--- booking/cancel.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Página de cancelamento do Stripe
-
-echo "<h1>🚫 Payment Cancelled</h1>";
-echo "<p>Your payment has been cancelled. You can return to your booking and try again.</p>";
-echo '<a href="/booking/">← Return to Booking</a>';
-?>
