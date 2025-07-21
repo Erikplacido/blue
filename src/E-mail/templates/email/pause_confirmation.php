25507348f70051227d076e63ebf9f5d2df947521
Erikplacido <erik.placido@gmail.com>
2025-07-21 09:11:47 -0300

    Delete src directory

--- src/E-mail/templates/email/pause_confirmation.php
+++
@@ -1,4 +0,0 @@
-<!-- /templates/email/pause_confirmation.php -->
-<p>Olá <?= htmlspecialchars($customerName) ?>,</p>
-<p>Sua assinatura foi pausada por 1 mês. Retomaremos as cobranças em <?= date('d/m/Y', strtotime('+1 month')) ?>.</p>
-<p>Qualquer dúvida, estamos à disposição.</p>
